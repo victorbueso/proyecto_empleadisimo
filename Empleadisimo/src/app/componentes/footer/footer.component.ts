8850cb0f4be10a897afc6b35d31fdcf80fd6ba5a
Alexander10531 <AlexanderTejedaBarahona10@gmail.com>
2021-02-12 17:29:12 -0600

    Correciones de footer (beta).

--- Empleadisimo/src/app/componentes/footer/footer.component.ts
+++ Empleadisimo/src/app/componentes/footer/footer.component.ts
@@ -2,14 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  templateUrl: './footer.component.html'
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent{
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
