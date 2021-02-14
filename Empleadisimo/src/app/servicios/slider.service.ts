1d500ed10a78aef94e8e9ea7c5a89ed103197d6b
Alexander10531 <AlexanderTejedaBarahona10@gmail.com>
2021-02-14 12:03:16 -0600

    Cambio de fotos.

--- Empleadisimo/src/app/servicios/slider.service.ts
+++ Empleadisimo/src/app/servicios/slider.service.ts
@@ -9,32 +9,25 @@ export class SliderService {
   slider: sliderData[] = [
     {
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, officiis repellendus eaque, doloremque exercitationem adipisci accusamus cupiditate eius quia facilis nostrum voluptatibus esse quidem harum molestias est sit? Voluptate, quod.",
-      classText: "main-title",
+      classText: "secondary-title",
       position: 0,
       title: "Lorem, ipsum dolor",
       img: "assets/img/pexels-thisisengineering-3861958.jpg",
     },
     {
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, officiis repellendus eaque, doloremque exercitationem adipisci accusamus cupiditate eius quia facilis nostrum voluptatibus esse quidem harum molestias est sit? Voluptate, quod.",
-      classText: "secondary-title",
+      classText: "main-title",
       position: 100,
       title: "Lorem, ipsum dolor",
-      img: "assets/img/pexels-thisisengineering-3861958.jpg",
-    },
-    {
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, officiis repellendus eaque, doloremque exercitationem adipisci accusamus cupiditate eius quia facilis nostrum voluptatibus esse quidem harum molestias est sit? Voluptate, quod.",
-      classText: "main-title",
-      position: 200,
-      title: "Lorem, ipsum dolor",
-      img: "assets/img/pexels-thisisengineering-3861958.jpg",
+      img: "assets/img/pexels-thisisengineering-3862135.jpg",
     },
     {
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, officiis repellendus eaque, doloremque exercitationem adipisci accusamus cupiditate eius quia facilis nostrum voluptatibus esse quidem harum molestias est sit? Voluptate, quod.",
       classText: "secondary-title",
-      position: 300,
+      position: 200,
       title: "Lorem, ipsum dolor",
-      img: "assets/img/pexels-thisisengineering-3861958.jpg",
-    }
+      img: "assets/img/pexels-anthony-shkraba-4348401.jpg",
+    },
   ]
 
   constructor(
