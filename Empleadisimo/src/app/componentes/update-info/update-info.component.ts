109d649eab1c1d3a6f62cce9c8728e3eb9dd949f
Alexander10531 <AlexanderTejedaBarahona10@gmail.com>
2021-02-23 21:16:56 -0600

    Agregado de estilos y validaciones para la actualizacion de la informacion

--- Empleadisimo/src/app/componentes/update-info/update-info.component.ts
+++ Empleadisimo/src/app/componentes/update-info/update-info.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-info',
   templateUrl: './update-info.component.html'
 })
-export class UpdateInfoComponent{
+export class UpdateInfoComponent implements OnInit{
+  
+  forma: FormGroup = this.fb.group({
+    name: [null, [Validators.required, Validators.minLength(3)]],
+    phone: [null, [Validators.required]],
+    gender: [null, [Validators.required]],
+    birthDate:[null, [Validators.required]]
+  })
+  
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(){
+  }
 
+  invalidName(){
+    return this.forma.get('name')?.invalid && this.forma.get('name')?.touched; 
+  }
 
+  invalidPhone(){
+    // console.log("Se esta ejecutando la funcion")
+    return this.forma.get('phone')?.invalid && this.forma.get('phone')?.touched;
+  }
+
+  invalidSex(){
+    return this.forma.get('gender')?.invalid && this.forma.get('gender')?.touched;
+  }
+  
+  invalidBirth(){
+    return this.forma.get('birthDate')?.invalid && this.forma.get('birthDate')?.touched;
+  }
+
+  save(){
+    console.log(this.forma.value)
+  }
 }
