b9b2d559039145beb63480db19eaf196fd563938
Alexander10531 <AlexanderTejedaBarahona10@gmail.com>
2021-02-26 19:38:45 -0600

    Correcciones de pantalla de actualizacion de perfil

--- Empleadisimo/src/app/componentes/update-info/update-info.component.ts
+++ Empleadisimo/src/app/componentes/update-info/update-info.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, PatternValidator, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-update-info',
@@ -8,10 +8,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class UpdateInfoComponent implements OnInit{
   
   forma: FormGroup = this.fb.group({
-    name: [null, [Validators.required, Validators.minLength(3)]],
-    phone: [null, [Validators.required]],
-    gender: [null, [Validators.required]],
-    birthDate:[null, [Validators.required]]
+
+    name: [null, [Validators.required, Validators.pattern("[a-zA-Z\\s]{3,}")]],
+    phone: [null, [Validators.required, Validators.pattern("^\\d{4}\\-?\\d{4}$")]],
+    birthDate:[null, [Validators.required]],
+    gender: [null, [Validators.required]]
+  
   })
   
   constructor(private fb: FormBuilder) {}
@@ -35,8 +37,26 @@ export class UpdateInfoComponent implements OnInit{
   invalidBirth(){
     return this.forma.get('birthDate')?.invalid && this.forma.get('birthDate')?.touched;
   }
-
-  save(){
-    console.log(this.forma.value)
+  
+  formatPhone(elementHtml: any){
+    if(this.forma.get('phone')?.valid){
+      if(elementHtml.value[4] != "-"){
+        elementHtml.value =  elementHtml.value.slice(0,4) + '-' + elementHtml.value.slice(4,)
+      }
+    }
   }
+  //  dateValidator(control: FormControl): ValidatorFn {
+  //   let dateT = new Date(control.value)
+  //   let dateB = new Date()
+  //   let age = Math.abs(dateT.getFullYear() - dateB.getFullYear());
+  //   if(age < 18){
+  //     return {
+  //       dateValidator: true
+  //     }
+  //   }
+  //   return null;
+  // }
+  save(){
+    console.log("Estorbas")
+  }  
 }
