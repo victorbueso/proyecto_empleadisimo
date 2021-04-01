31e6ebd5e6e7e5d42c65e0bfc8d6a1edd03141dc
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2021-03-31 20:11:50 -0600

    Sección de empleados del lado de Admin en proceso

--- Empleadisimo/src/app/admin/admin.module.ts
+++ Empleadisimo/src/app/admin/admin.module.ts
@@ -10,19 +10,37 @@ import { CompaniesComponent } from './companies/companies.component';
 import { HomeComponent } from './home/home.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PaginationPipe } from './pipes/pagination.pipe';
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatPaginatorIntl, MatPaginatorModule} from '@angular/material/paginator'
+import { CustomMatPaginatorIntl } from './paginator-admin';
 
 
 
 @NgModule({
-  declarations: [AdminsComponent, SidebarComponent, EmployeesComponent, CompaniesComponent, HomeComponent, DashboardComponent],
+  declarations: [ AdminsComponent, 
+    SidebarComponent, 
+    EmployeesComponent, 
+    CompaniesComponent, 
+    HomeComponent, 
+    DashboardComponent,
+    PaginationPipe],
   imports: [
     CommonModule,
     AdminRoutingModule,
     FontAwesomeModule,
     FormsModule,
     ReactiveFormsModule,
-    NgbModule
+    NgbModule,
+    MatTabsModule,
+    MatPaginatorModule
+  ], 
+  providers:[
+    {
+      provide: MatPaginatorIntl,
+      useClass : CustomMatPaginatorIntl
+    }
   ]
 })
 export class AdminModule { }
